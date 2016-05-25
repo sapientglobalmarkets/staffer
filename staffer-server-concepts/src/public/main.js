c3723ee3f7bf101f34971cc164cd0ad9b796ed8a
nareshbhatia <naresh.archfirst@gmail.com>
2016-05-25 09:49:46 -0400

    Added full data to falcor model

--- staffer-server-concepts/src/public/main.js
+++ staffer-server-concepts/src/public/main.js
@@ -13,10 +13,22 @@ model.get(['needMap', needId, ['startDate', 'endDate']])
 // model.get([
 //         'needs',
 //         {from: 0, to: 100},
-//         ['startDate', 'endDate', 'project', 'skill', 'person'],
+//         ['id', 'startDate', 'endDate', 'project', 'skill', 'person'],
 //         ['name', 'email', 'phone']
 //     ])
 // .then(function(response) {
 //     console.log(JSON.stringify(response, null, 4));
+
+//     Object.keys(response.json.needs).forEach(function(key) {
+//         var need = response.json.needs[key];
+
+//         console.log(
+//             need.skill.name + ', ' +
+//             need.project.name + ', ' +
+//             need.startDate + ', ' +
+//             need.endDate + ', ' +
+//             (need.person ? need.person.name : '-'));
+//     });
+
 //     return 0;
 // });
